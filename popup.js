021190fa19fc830dfe1688c4a699199fa17993da
Brandon Bevans <brandonbevans@gmail.com>
2016-02-03 16:26:06 -0800

    The extension can now interact with the windows dom

--- popup.js
+++ popup.js
@@ -8,6 +8,9 @@
  * @param {function(string)} callback - called when the URL of the current tab
  *   is found.
  */
+
+
+
 function getCurrentTabUrl(callback) {
   // Query filter to be passed to chrome.tabs.query - see
   // https://developer.chrome.com/extensions/tabs#method-query
@@ -47,78 +50,59 @@ function getCurrentTabUrl(callback) {
   // alert(url); // Shows "undefined", because chrome.tabs.query is async.
 }
 
-/**
- * @param {string} searchTerm - Search term for Google Image search.
- * @param {function(string,number,number)} callback - Called when an image has
- *   been found. The callback gets the URL, width and height of the image.
- * @param {function(string)} errorCallback - Called when the image is not found.
- *   The callback gets a string that describes the failure reason.
- */
-function getImageUrl(searchTerm, callback, errorCallback) {
-  // Google image search - 100 searches per day.
-  // https://developers.google.com/image-search/
-  var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' +
-    '?v=1.0&q=' + encodeURIComponent(searchTerm);
-  var x = new XMLHttpRequest();
-  x.open('GET', searchUrl);
-  // The Google image search API responds with JSON, so let Chrome parse it.
-  x.responseType = 'json';
-  x.onload = function() {
-    // Parse and process the response from Google Image Search.
-    var response = x.response;
-    if (!response || !response.responseData || !response.responseData.results ||
-        response.responseData.results.length === 0) {
-      errorCallback('No response from Google Image search!');
-      return;
-    }
-    var firstResult = response.responseData.results[0];
-    // Take the thumbnail instead of the full image to get an approximately
-    // consistent image size.
-    var imageUrl = firstResult.tbUrl;
-    var width = parseInt(firstResult.tbWidth);
-    var height = parseInt(firstResult.tbHeight);
-    console.assert(
-        typeof imageUrl == 'string' && !isNaN(width) && !isNaN(height),
-        'Unexpected respose from the Google Image Search API!');
-    callback(imageUrl, width, height);
-  };
-  x.onerror = function() {
-    errorCallback('Network error.');
-  };
-  x.send();
-}
-
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
-}
-
 document.addEventListener('DOMContentLoaded', function() {
 
-  var ws = new WebSocket("ws://localhost:10000");
-  //socket.connect('http://localhost:10000', {autoConnect : true});
+	var ws = new WebSocket("ws://localhost:10000");
+	$('#b1').click(function() { 
+		console.log('clicked button 1') 
+		ws.send("option1")
+	})
 
-  console.log('check');
-    /*
-    // Put the image URL in Google search.
-    renderStatus('Performing Google Image search for ' + url);
+	$('#b2').click(function() {
+		console.log('clicked button 2')
+		ws.send("option2")
 
-    getImageUrl(url, function(imageUrl, width, height) {
+	})
 
-      renderStatus('Search term: ' + url + '\n' +
-          'Google image search result: ' + imageUrl);
-      var imageResult = document.getElementById('image-result');
-      // Explicitly set the width/height to minimize the number of reflows. For
-      // a single image, this does not matter, but if you're going to embed
-      // multiple external images in your page, then the absence of width/height
-      // attributes causes the popup to resize multiple times.
-      imageResult.width = width;
-      imageResult.height = height;
-      imageResult.src = imageUrl;
-      imageResult.hidden = false;
+	$('#b3').click(function() {
+		console.log('clicked button 3')
+		ws.send("option3")
 
-    }, function(errorMessage) {
-      renderStatus('Cannot display image. ' + errorMessage);
-    });
-    */
+	})
+
+	$('#b4').click(function() {
+		console.log('clicked button 4')
+		ws.send("option4")
+	})
+
+	//socket.connect('http://localhost:10000', {autoConnect : true});
+
+	console.log('check');
 
 });
+
+
+//Grab the current pages HTML
+
+chrome.runtime.onMessage.addListener(function(request, sender) {
+  if (request.action == "getSource") {
+    message.innerText = request.source;
+  }
+});
+
+function onWindowLoad() {
+
+  var message = document.querySelector('#message');
+
+  chrome.tabs.executeScript(null, {
+    file: "getPagesSource.js"
+  }, function() {
+    // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+    if (chrome.runtime.lastError) {
+      message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+    }
+  });
+
+}
+
+window.onload = onWindowLoad;
--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 import asyncio
 import websockets
 
-async def hello(websocket, path):
-	print('func called..')
+async def clientConnected(websocket, path):
+	print('client connected..')
+	while True:
+		message = await websocket.recv()
+		print (message)
 
-start_server = websockets.serve(hello, 'localhost', 10000)
+start_server = websockets.serve(clientConnected, 'localhost', 10000)
 print('ran the server..')
 asyncio.get_event_loop().run_until_complete(start_server)
 
