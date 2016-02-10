6c2a01760c6a795dfb09f5d6ea998ece749138f7
David Scrobonia <davidscrobonia@gmail.com>
2016-02-10 15:02:26 -0800

    some stuff

--- popup.js
+++ popup.js
@@ -95,7 +95,7 @@ function onWindowLoad() {
   var message = document.querySelector('#message');
 
   chrome.tabs.executeScript(null, {
-    file: "getPagesSource.js"
+    file: "findVulnerabilities.js"
   }, function() {
     // If you try and inject into an extensions page or the webstore/NTP you'll get an error
     if (chrome.runtime.lastError) {
