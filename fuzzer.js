53471955b0f6023f013bc2c494ffde859cb2be9e
David Scrobonia <davidscrobonia@gmail.com>
2016-04-19 13:39:51 -0700

    made a fuzzer class i guess?

--- fuzzer.js
+++ fuzzer.js
@@ -1,3 +1,15 @@
-function fuzzer() {
-	
+function fuzzer(base_url, fuzz_param, other_params) {
+	var req = new XMLHttpRequest();
+    var url = base_url + "?" + fuz
+
+    for (thing in file) {
+	    req.open('GET', document.location, false);
+	    req.send(null);
+	    headers = req.getAllResponseHeaders().toLowerCase();
+
+	    if (headers.length > 0)
+	         headers = '\nHeader Information:\n' + headers;
+	}
+
+    
 }
